1ff29c2b748fc7eed5fd242dd14e4d1e7c3e8472
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-07-13 10:42:30 -0400

    Place markers at coords

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
-const axios = require('axios')
+import axios from 'axios'
 
-axios.get('https://public.enigma.com/api/snapshots/a4bac19c-e333-45f8-a96e-1082d069217f?&query_mode=advanced&query=(antarctic)&row_limit=200&row_offset=0&include_serialids=true')
+const url = 'https://public.enigma.com/api/snapshots/a4bac19c-e333-45f8-a96e-1082d069217f?&query_mode=advanced&query=(antarctic)&row_limit=200&row_offset=0&include_serialids=true'
+const coordListPromise = axios.get(url)
 .then(res => res.data)
 .then(data => data.table_rows)
 .then(table_rows => table_rows.rows)
@@ -8,4 +9,6 @@ axios.get('https://public.enigma.com/api/snapshots/a4bac19c-e333-45f8-a96e-1082d
 	row[42], row[43]
 ]))
 // .then(rows => rows[0].forEach((field, idx) => console.log(idx, field)))
-.then(rows => console.log(rows))
+// .then(rows => console.log(rows))
+
+export default coordListPromise
